94112dec237f0547be5d56e37b6a102df02de76e
juhoaws <jjantun@amazon.com>
2023-12-22 10:11:34 -0800

    Latency measurements for the Unity GameLift integration

--- UnitySample/Assets/BackendFeatures/AmazonGameLiftIntegration/AmazonGameLiftIntegration.cs
+++ UnitySample/Assets/BackendFeatures/AmazonGameLiftIntegration/AmazonGameLiftIntegration.cs
@@ -7,6 +7,9 @@ using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
 using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
 
 public class MatchmakingRequestData
 {
@@ -29,14 +32,84 @@ public class AmazonGameLiftIntegration : MonoBehaviour
     public string gameliftIntegrationBackendEndpointUrl;
     public Text logOutput;
 
+
     // Total tries of trying to get match status
     private int totalMatchStatusRequests = 0;
 
     private string ticketId = null;
 
+    // Latencies
+    private Dictionary<string, double> latencies = new Dictionary<string, double>();
+
+    async Task<double> SendHTTPSPingRequest(string requestUrl)
+    {
+        try {
+            //First request to establish the connection
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri(requestUrl),
+            };
+            // Execute the request
+            var client = new HttpClient();
+            var resp = await client.SendAsync(request);
+
+            // We measure the average of second and third requests to get the TCP latency without HTTPS handshake
+            var startTime = DateTime.Now;
+            request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri(requestUrl),
+            };
+            resp = await client.SendAsync(request);
+            request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri(requestUrl),
+            };
+            resp = await client.SendAsync(request);
+            // Total time
+            var totalTime = (DateTime.Now - startTime).TotalMilliseconds / 2.0;
+            return totalTime;
+        }
+        catch(Exception e) {
+            print("Error reaching the endpoint " + requestUrl + ", setting latency to 1 second");
+            return 1000.0;
+        }
+    }
+
+    // Helper method to measure TCP latencies to the two sample locatiosn we use
+    void MeasureLatencies()
+    {
+        // We'll ping the two Regions we are using, you can extend to any amount
+        var region1 = "us-east-1";
+        var region2 = "us-west-2";
+
+        // Check latencies to Regions by pinging DynamoDB endpoints (they just report health but we use them here for latency)
+        var response = Task.Run(() => this.SendHTTPSPingRequest("https://dynamodb."+ region1 + ".amazonaws.com"));
+        response.Wait(1000); // We'll expect a response in 1 second
+        string latency1 = "Latency " + region1 + ": " + response.Result + " ms";
+        print(latency1);
+        this.latencies.Add(region1, response.Result);
+        response = Task.Run(() => this.SendHTTPSPingRequest("https://dynamodb." + region2 + ".amazonaws.com"));
+        response.Wait(1000); // We'll expect a response in 1 second
+        string latency2 = "Latency " + region2 + ": " + response.Result + " ms";
+        print(latency2);
+        this.latencies.Add(region2, response.Result);
+
+        // Write latencies to logOutput
+        this.logOutput.text += latency1 + " ";
+        this.logOutput.text += latency2 + "\n";
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        // Measure the latencies first
+        Debug.Log("Measuring latencies");
+        this.logOutput.text += "Measuring latencies...\n";
+        this.MeasureLatencies();
+        
         // Set the login endpoint
         Debug.Log("Setting login endpoint");
         AWSGameSDKClient.Instance.Init(loginEndpointUrl, this.OnLoginOrRefreshError);
@@ -84,8 +157,18 @@ public class AmazonGameLiftIntegration : MonoBehaviour
         //PlayerPrefs.SetString("refresh_token_expires_datetime", DateTime.UtcNow.AddSeconds(userInfo.refresh_token_expires_in).ToString()); // You could use this to initialize new full login when running out of time
         PlayerPrefs.Save();
 
+        // Generate a latencyInMs json from the latencies we measured
+        string latencyInMs = "{ \"latencyInMs\": { ";
+        foreach(var latency in this.latencies)
+        {
+            latencyInMs += "\"" + latency.Key + "\": " + (int)latency.Value + ", ";
+        }
+        latencyInMs = latencyInMs.Substring(0, latencyInMs.Length - 2);
+        latencyInMs += " } }";
+        Debug.Log("LatencyInMs: " + latencyInMs);
+
         // POST a requestMatchmaking with latency information
-        AWSGameSDKClient.Instance.BackendPostRequest(this.gameliftIntegrationBackendEndpointUrl, "request-matchmaking", this.OnRequestMatchmakingResponse, "{ \"latencyInMs\": { \"us-east-1\": 60, \"us-west-2\": 150 } }");
+        AWSGameSDKClient.Instance.BackendPostRequest(this.gameliftIntegrationBackendEndpointUrl, "request-matchmaking", this.OnRequestMatchmakingResponse, latencyInMs);
     }
 
     void OnRequestMatchmakingResponse(UnityWebRequest response)
@@ -182,6 +265,8 @@ public class AmazonGameLiftIntegration : MonoBehaviour
                 this.logOutput.text += "Received message from server: " + message + "\n";
                 break;
             }
+            
+            // Wait just 50ms as we'll get the response soon
             yield return new WaitForSeconds(0.05f);
         }
     }
