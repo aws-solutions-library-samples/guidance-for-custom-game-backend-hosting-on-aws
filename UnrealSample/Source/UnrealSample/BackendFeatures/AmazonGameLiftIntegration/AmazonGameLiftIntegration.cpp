9ea29c4f738641d8385e376aa544ab67076a5506
juhoaws <jjantun@amazon.com>
2023-12-26 10:42:30 -0800

    Initial Unreal setup for GameLift integration

---
+++ UnrealSample/Source/UnrealSample/BackendFeatures/AmazonGameLiftIntegration/AmazonGameLiftIntegration.cpp
@@ -0,0 +1,108 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved. SPDX-License-Identifier: MIT-0
+
+
+#include "AmazonGameLiftIntegration.h"
+#include "../../AWSGameSDK/AWSGameSDK.h"
+#include "../../PlayerDataSave.h"
+#include "Engine/World.h"
+#include "Kismet/GameplayStatics.h"
+#include "../../PlayerDataManager.h"
+
+// Sets default values for this component's properties
+UAmazonGameLiftIntegration::UAmazonGameLiftIntegration()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+// Called when the game starts
+void UAmazonGameLiftIntegration::BeginPlay()
+{
+	Super::BeginPlay();
+
+    // Get the subsystems
+    UGameInstance* GameInstance = Cast<UGameInstance>(UGameplayStatics::GetGameInstance(GetWorld()));
+    UAWSGameSDK* AWSGameSDK =  GameInstance->GetSubsystem<UAWSGameSDK>();
+    UPlayerDataManager* PlayerDataManager = GameInstance->GetSubsystem<UPlayerDataManager>(); 
+    
+    // Init with the login endpoint defined in the Editor and a callback to handle errors for logging in and refresh
+	AWSGameSDK->Init(this->m_loginEndpoint);
+	AWSGameSDK->OnLoginFailure.AddUObject(this, &UAmazonGameLiftIntegration::OnLoginOrRefreshErrorCallback);
+
+    // Define the OnLoginResult callback
+	UAWSGameSDK::FLoginComplete loginCallback;
+	loginCallback.BindUObject(this, &UAmazonGameLiftIntegration::OnLoginResultCallback);
+    
+    // Get player data if we have any 
+    auto playerData = PlayerDataManager->LoadGameData();
+
+    // If not saved player data, login as a new player
+    if(playerData == nullptr){
+        UE_LOG(LogTemp, Display, TEXT("No player data yet, request a new identity"));
+        if(GEngine)
+            GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Black, TEXT("No player data yet, request a new identity"));	
+
+        // Login as a guest user
+        AWSGameSDK->LoginAsNewGuestUser(loginCallback);
+    }
+    else {
+        UE_LOG(LogTemp, Display, TEXT("Existing player data\n user_id: %s \n guest_secret: %s"), *playerData->UserId, *playerData->GuestSecret);
+        if(GEngine)
+            GEngine->AddOnScreenDebugMessage(-1, 30.0f, FColor::Black, FString::Printf(TEXT("Existing player data\n user_id: %s \n guest_secret: %s"), *playerData->UserId, *playerData->GuestSecret),false, FVector2D(1.5f,1.5f));
+
+        AWSGameSDK->LoginAsGuestUser(playerData->UserId, playerData->GuestSecret, loginCallback);
+    }
+}
+
+// Called when there is an error with login or token refresh. You will need to handle logging in again here
+void UAmazonGameLiftIntegration::OnLoginOrRefreshErrorCallback(const FString& errorMessage){
+    UE_LOG(LogTemp, Display, TEXT("Received login error: %s \n"), *errorMessage);
+    if(GEngine)
+        GEngine->AddOnScreenDebugMessage(-1, 30.0f, FColor::Red, FString::Printf(TEXT("Received login error: \n %s \n"), *errorMessage), false, FVector2D(1.5f,1.5f));
+
+    // NOTE:  You will need to handle logging in again here
+}
+
+// Called when login is done
+void UAmazonGameLiftIntegration::OnLoginResultCallback(const UserInfo& userInfo){
+    UE_LOG(LogTemp, Display, TEXT("Received login response: %s \n"), *userInfo.ToString());
+    if(GEngine)
+            GEngine->AddOnScreenDebugMessage(-1, 30.0f, FColor::Black, FString::Printf(TEXT("Received login response: \n %s \n"), *userInfo.ToString()), false, FVector2D(1.5f,1.5f));
+
+    // Save the player data
+    UGameInstance* GameInstance = Cast<UGameInstance>(UGameplayStatics::GetGameInstance(GetWorld()));
+    UPlayerDataManager* PlayerDataManager = GameInstance->GetSubsystem<UPlayerDataManager>(); 
+    PlayerDataManager->SaveGameData(userInfo.user_id, userInfo.guest_secret);
+
+    // NOTE: You could get the expiration in seconds for refresh token (and modify to FDateTime) as well as the refresh token itself from the userInfo,
+    // and login next time with the refresh token itself. This can be done by calling AWSGameSDK->LoginWithRefreshToken(refreshToken, loginCallback);
+
+    // Test calling our custom backend system to set player data
+	UAWSGameSDK::FRequestComplete requestMatchmakingCallback;
+	requestMatchmakingCallback.BindUObject(this, &UAmazonGameLiftIntegration::OnRequestMatchmakingResponse);
+    UAWSGameSDK* AWSGameSDK =  GameInstance->GetSubsystem<UAWSGameSDK>();
+	// TODO: POST request to matchmaking backend
+    //AWSGameSDK->BackendGetRequest(this->m_gameliftIntegrationBackendEndpointUrl, "request-matchmaking", params, setPlayerDataCallback);
+}
+
+// Callback for matchmaking request
+void UAmazonGameLiftIntegration::OnRequestMatchmakingResponse(const FString& response){
+	UE_LOG(LogTemp, Display, TEXT("Received matchmaking response: %s \n"), *response);
+}
+
+// Callback for match status request
+void UAmazonGameLiftIntegration::OnGetMatchStatusResponse(const FString& response){
+	UE_LOG(LogTemp, Display, TEXT("Received match status response: %s \n"), *response);
+}
+
+// Called every frame
+void UAmazonGameLiftIntegration::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
