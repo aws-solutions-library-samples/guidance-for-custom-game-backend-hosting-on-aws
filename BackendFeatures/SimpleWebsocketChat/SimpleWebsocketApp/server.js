c55f9a9b6d6268403fcc65710535cdebb6375584
juhoaws <jjantun@amazon.com>
2024-05-13 12:22:46 -0700

    Websockets: Split out redis management to a separate class

--- BackendFeatures/SimpleWebsocketChat/SimpleWebsocketApp/server.js
+++ BackendFeatures/SimpleWebsocketChat/SimpleWebsocketApp/server.js
@@ -1,9 +1,4 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: MIT-0
-
 'use strict';
-
-
 
 // X-ray for distributed tracing
 var AWSXRay = require('aws-xray-sdk');
@@ -21,61 +16,9 @@ const verifier = JwtRsaVerifier.create({
   scope: ["guest", "authenticated"], // We accept guest and authenticated scope
 });
 
-// Redis client for our ElastiCache Serverless endpoint
-const redis = require('redis');
-const redisClient = redis.createClient({
-  socket: {
-    host: process.env.REDIS_ENDPOINT,
-    port: 6379,
-    tls: true
-  }
-});
-redisClient.connect();
-
-// Redis client for pubSub
-const redisPubSubClient = redis.createClient({
-  socket: {
-    host: process.env.REDIS_ENDPOINT,
-    port: 6379,
-    tls: true
-  }
-});
-redisPubSubClient.connect();
-
-redisClient.on('error', (err) => {
-  console.error('Redis error:', err);
-});
-
-redisClient.on('end', () => {
-  console.log('Redis connection closed');
-});
-
-redisClient.on('reconnecting', () => {
-  console.log('Reconnecting to Redis...');
-});
-
-redisPubSubClient.on('error', (err) => {
-  console.error('Redis error:', err);
-});
-
-redisPubSubClient.on('end', () => {
-  console.log('Redis connection closed');
-});
-
-redisPubSubClient.on('reconnecting', () => {
-  console.log('Reconnecting to Redis...');
-});
-
-// Websockets mapped to userIDs
-const websockets = new Map();
-
-// Map of maps of websockets mapped to channels
-const channelSubscriptions = new Map();
-
-// WEBSOCKET SERVER on 80
-const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: 80 });
-const url = require('url');
+// Redis manager
+const RedisManager = require('./RedisManager');
+const redisManager = new RedisManager(process.env.REDIS_ENDPOINT);
 
 // Callback for pub/sub chat message handling
 const listener = (message, channel) => {
@@ -83,11 +26,15 @@ const listener = (message, channel) => {
   console.log(message, channel);
 
   // Send the message to all websockets subscribed to this channel
-  const channelSubscribers = channelSubscriptions.get(channel);
+  const channelSubscribers = redisManager.channelSubscriptions.get(channel);
   if (channelSubscribers) {
     channelSubscribers.forEach((ws) => {
       try {
-        ws.send(JSON.stringify({ type: "chat_message_received", payload: { message: message, channel: channel } }));
+        // Get the message and user name from the JSON message
+        const messageParsed = JSON.parse(message);
+        const username = messageParsed.username;
+        const messageText = messageParsed.message;
+        ws.send(JSON.stringify({ type: "chat_message_received", payload: { username: username, message: messageText, channel: channel } }));
       } catch (err) {
         console.error("Error sending chat message over websocket: " + err);
       }
@@ -95,8 +42,12 @@ const listener = (message, channel) => {
   }
 };
 
-wss.on('connection', async (ws, req) => {
+// WEBSOCKET SERVER on 80
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 80 });
+const url = require('url');
 
+wss.on('connection', async (ws, req) => {
   console.log("New websocket connection");
 
   const params = url.parse(req.url, true);
@@ -106,12 +57,12 @@ wss.on('connection', async (ws, req) => {
     ws.send(JSON.stringify({ error: "No authentication token provided" }));
     ws.close();
   }
-  
+
   try {
     var payload = await verifier.verify(params.query.auth_token);
     console.log("Token is valid");
     // Add the user to the websocket map
-    websockets.set(ws, payload.sub);
+    redisManager.addWebsocket(ws, payload.sub);
 
   } catch (err) {
     console.log(err);
@@ -131,142 +82,77 @@ wss.on('connection', async (ws, req) => {
   // Callback for disconnecting
   ws.on('close', function close() {
     try {
-      console.log('User disconnected');
-      // Remove user from all subscriptions
-      const channelsToRemove = new Set();
-      channelSubscriptions.forEach((subscriberMap, key) => {
-        subscriberMap.delete(ws);
-        // If channel is empty, mark for removal
-        if (subscriberMap.size === 0) {
-          channelsToRemove.add(key);
-        }
-      });
-
-      // Unsubscribe the redis client from channels with no more subscribers
-      channelsToRemove.forEach((channel) => {
-        redisPubSubClient.sUnsubscribe(channel, listener);
-        console.log("No more people on channel, Unsubscribed server from " + channel);
-        channelSubscriptions.delete(channel);
-      });
-
-      // Remove the user from the websocket map
-      websockets.delete(ws);
+      redisManager.removeWebsocket(ws);
     } catch (err) {
       console.log("Error disconnecting user: " + err);
     }
   });
-
 });
 
 // Message handler function for messages from client through the Websocket
 async function handleMessage(ws, data) {
+  try {
+    console.log('received: %s', data);
 
-    try {
-      console.log('received: %s', data);
+    const dataString = data.toString();
+    const parsedData = JSON.parse(dataString);
 
-      // Get the userID from the websocket map
-      const userID = websockets.get(ws);
+    // Check message type
 
-      const dataString = data.toString();
-      const parsedData = JSON.parse(dataString);
-
-      // Check message type
-
-      // Set new name for the user
-      if (parsedData.type === "set-name") {
-          // Set the user's name in Redis
-          const username = parsedData.payload.username;
-          // log the userID and username
-          console.log("Setting username for " + userID + " to " + username);
-          redisClient.set(userID, username);
-          // Send a message back to the client
-          ws.send(JSON.stringify({ message: `Username set to ${username}` }));
-      }
-      
-      // Subscribe to a channel
-      else if (parsedData.type === "join") {
-          // Check if we subscribed already and if not, add to the list
-          const channel = parsedData.payload.channel;
-          // log the userID and channel
-          console.log("Subscribing " + userID + " to " + channel);
-          // add the user to the channel's list of subscribers and create one if it doesn't exist
-          if (!channelSubscriptions.has(channel)) {
-              console.log("Channel subscription not set yet on this server, creating..");
-              channelSubscriptions.set(channel, new Set());
-              // subscribe the redis client to the channel
-              redisPubSubClient.sSubscribe(channel, listener);
-              console.log("Done!");
-          }
-          // add the websocket to the channel's list of subscribers if it's not already there
-          if (!channelSubscriptions.get(channel).has(ws)) {
-            channelSubscriptions.get(channel).add(ws);
-            ws.send(JSON.stringify({ message: `You have joined ${channel}` }));
-          }
-          else {
-            ws.send(JSON.stringify({ message: `You have already joined ${channel}` }));
-          }
-      }
-
-      // Unsubscribe from a channel
-      else if (parsedData.type === "leave") {
-          // Get the channel we're unsubscribing from from data
-          const channel = parsedData.payload.channel;
-          // log the userID and channel
-          console.log("Unsubscribing " + userID + " from " + channel);
-          // remove the user from the channel's list of subscribers
-          channelSubscriptions.get(channel).delete(ws);
-          // if there are no more subscribers, unsubscribe from the channel
-          if (channelSubscriptions.get(channel).size === 0) {
-              console.log("No more subscribers, unsubscribing..");
-              // unsubscribe the redis client from the channel
-              redisPubSubClient.sUnsubscribe(channel, listener);
-              console.log("Done!");
-              // remove the channel from the map
-              channelSubscriptions.delete(channel);
-          }
-          ws.send(JSON.stringify({ message: `You have left ${channel}` }));
-      }
-
-      // Receive message to a channel
-      else if (parsedData.type === "message") {
-          const username = await redisClient.get(userID);
-
-          // log the datastring and ws
-          console.log("Message received: " + dataString + " from " + ws);
-          // Get the channel we're sending to from data
-          const channel = parsedData.payload.channel;
-          // Get the message from data
-          const message = parsedData.payload.message;
-
-          if (!username) {
-              ws.send(JSON.stringify({ error: "You must set a username first" }));
-              return;
-          }
-          // Check that this websocket is subscribed to the channel
-          if (!channelSubscriptions.has(channel)) {
-              ws.send(JSON.stringify({ error: "You must join the channel first" }));
-              return;
-          }
-          else if(!channelSubscriptions.get(channel).has(ws)) {
-            ws.send(JSON.stringify({ error: "You must join the channel first" }));
-            return;
-          }
-          
-          // Publish to channel
-          redisClient.publish(channel, username + ": " + message);
-          // Send response to client
-          ws.send(JSON.stringify({ message: `Message sent to ${channel}: ${message}` }));
-      }
-      
-      // Any other messages
-      else {
-          ws.send(JSON.stringify({ error: "Invalid message" }));
-      }
-    } catch (err) {
-      console.log(err);
-      ws.send(JSON.stringify({ error: `Error handling message: ${err}` }));
+    // Set new name for the user
+    if (parsedData.type === "set-name") {
+      const username = parsedData.payload.username;
+      const userID = redisManager.websockets.get(ws);
+      redisManager.setUsername(userID, username);
+      ws.send(JSON.stringify({ message: `Username set to ${username}` }));
     }
+
+    // Subscribe to a channel
+    else if (parsedData.type === "join") {
+      const channel = parsedData.payload.channel;
+      redisManager.subscribeToChannel(channel, ws, listener);
+    }
+
+    // Unsubscribe from a channel
+    else if (parsedData.type === "leave") {
+      const channel = parsedData.payload.channel;
+      redisManager.unsubscribeFromChannel(channel, ws);
+    }
+
+    // Receive message to a channel
+    else if (parsedData.type === "message") {
+      const userID = redisManager.websockets.get(ws);
+      const username = await redisManager.getUsername(userID);
+
+      console.log(`Message received: ${dataString} from ${ws}`);
+      const channel = parsedData.payload.channel;
+      const message = parsedData.payload.message;
+
+      if (!username) {
+        ws.send(JSON.stringify({ error: "You must set a username first" }));
+        return;
+      }
+      if (!redisManager.channelSubscriptions.has(channel) || !redisManager.channelSubscriptions.get(channel).has(ws)) {
+        ws.send(JSON.stringify({ error: "You must join the channel first" }));
+        return;
+      }
+
+      const messageToPublish = JSON.stringify({ username, message });
+      redisManager.publishToChannel(channel, messageToPublish);
+      ws.send(JSON.stringify({ message: `Message sent to ${channel}: ${message}` }));
+    }
+
+    // Any other messages
+    else {
+      ws.send(JSON.stringify({ error: "Invalid message" }));
+    }
+  } catch (err) {
+    console.log(err);
+    ws.send(JSON.stringify({ error: `Error handling message: ${err}` }));
+  }
 }
+
+// *********** //
 
 // HEALTH CHECK SERVER for load balancer on 8080
 
@@ -283,7 +169,7 @@ app.use(AWSXRay.express.openSegment('SimpleWebsocketChat-HealthCheck'));
 // health check for root get
 app.get('/', (req, res) => {
   res.status(200).json({ statusCode: 200, message: "OK" });
-} );
+});
 app.use(AWSXRay.express.closeSegment());
 
 // Setup app
