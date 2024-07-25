1c6569e0b34f86f83a7e26d4240d81f42367e72c
juhoaws <60425750+juhoaws@users.noreply.github.com>
2024-07-25 21:16:17 +0000

    Update RedisManager.js with more error handling and unsubscribe on server disabled for now

--- BackendFeatures/SimpleWebsocketChat/SimpleWebsocketApp/RedisManager.js
+++ BackendFeatures/SimpleWebsocketChat/SimpleWebsocketApp/RedisManager.js
@@ -49,44 +49,68 @@ class RedisManager {
   }
 
   setUsername(userID, username) {
-    console.log(`Setting username for ${userID} to ${username}`);
-    this.redisClient.set(userID, username);
+    try {
+      console.log(`Setting username for ${userID} to ${username}`);
+      this.redisClient.set(userID, username);
+    } catch (err) {
+      console.log("Error in setUsername: " + err);
+    }
   }
 
   async getUsername(userID) {
-    return await this.redisClient.get(userID);
+    try {
+      return await this.redisClient.get(userID);
+    } catch (err) {
+      console.log("Error in getUsername: " + err);
+      return null
+    }
   }
 
   subscribeToChannel(channel, ws, listener) {
-    console.log(`Subscribing ${this.websockets.get(ws)} to ${channel}`);
-    if (!this.channelSubscriptions.has(channel)) {
-      console.log('Channel subscription not set yet on this server, creating...');
-      this.channelSubscriptions.set(channel, new Set());
-      this.redisPubSubClient.sSubscribe(channel, listener);
-      console.log('Done!');
-    }
-    if (!this.channelSubscriptions.get(channel).has(ws)) {
-      this.channelSubscriptions.get(channel).add(ws);
-      ws.send(JSON.stringify({ message: `You have joined ${channel}` }));
-    } else {
-      ws.send(JSON.stringify({ message: `You have already joined ${channel}` }));
+    try {
+      console.log(`Subscribing ${this.websockets.get(ws)} to ${channel}`);
+      if (!this.channelSubscriptions.has(channel)) {
+        console.log('Channel subscription not set yet on this server, creating...');
+        this.channelSubscriptions.set(channel, new Set());
+        this.redisPubSubClient.sSubscribe(channel, listener);
+        console.log('Done!');
+      }
+      if (!this.channelSubscriptions.get(channel).has(ws)) {
+        this.channelSubscriptions.get(channel).add(ws);
+        ws.send(JSON.stringify({ message: `You have joined ${channel}` }));
+      } else {
+        ws.send(JSON.stringify({ message: `You have already joined ${channel}` }));
+      }
+    } catch (err) {
+      console.log("Error in subscribe: " + err);
     }
   }
 
   unsubscribeFromChannel(channel, ws) {
-    console.log(`Unsubscribing ${this.websockets.get(ws)} from ${channel}`);
-    this.channelSubscriptions.get(channel).delete(ws);
-    if (this.channelSubscriptions.get(channel).size === 0) {
-      console.log('No more subscribers, unsubscribing...');
-      this.redisPubSubClient.sUnsubscribe(channel);
-      console.log('Done!');
-      this.channelSubscriptions.delete(channel);
+    try {
+      console.log(`Unsubscribing ${this.websockets.get(ws)} from ${channel}`);
+      this.channelSubscriptions.get(channel).delete(ws);
+      if (this.channelSubscriptions.get(channel).size === 0) {
+        console.log(`No more people on channel, Unsubscribe server from ${channel}`)
+        // NOTE: This is now disabled because of a rare error happening in the Redis client, see https://github.com/redis/node-redis/issues/2417 on a similar issue, you can fix this modifying the redis library
+        //this.redisPubSubClient.sUnsubscribe(channel); 
+        console.log('Done!');
+        this.channelSubscriptions.delete(channel);
+      }
+      ws.send(JSON.stringify({ message: `You have left ${channel}` }));
     }
-    ws.send(JSON.stringify({ message: `You have left ${channel}` }));
+    catch (err) {
+      console.log("Error in unsubscribe: " + err);
+    }
   }
 
   publishToChannel(channel, message) {
-    this.redisClient.publish(channel, message);
+    try {
+      this.redisClient.publish(channel, message);
+    }
+    catch (err) {
+      console.log("Error in publish: " + err);
+    }
   }
 
   addWebsocket(ws, userID) {
@@ -94,24 +118,30 @@ class RedisManager {
   }
 
   removeWebsocket(ws) {
-    const userID = this.websockets.get(ws);
-    this.websockets.delete(ws);
+    try {
+      const userID = this.websockets.get(ws);
+      this.websockets.delete(ws);
 
-    const channelsToRemove = new Set();
-    this.channelSubscriptions.forEach((subscriberMap, channel) => {
-      subscriberMap.delete(ws);
-      if (subscriberMap.size === 0) {
-        channelsToRemove.add(channel);
-      }
-    });
+      const channelsToRemove = new Set();
+      this.channelSubscriptions.forEach((subscriberMap, channel) => {
+        subscriberMap.delete(ws);
+        if (subscriberMap.size === 0) {
+          channelsToRemove.add(channel);
+        }
+      });
 
-    channelsToRemove.forEach((channel) => {
-      this.redisPubSubClient.sUnsubscribe(channel);
-      console.log(`No more people on channel, Unsubscribed server from ${channel}`);
-      this.channelSubscriptions.delete(channel);
-    });
+      channelsToRemove.forEach((channel) => {
+        console.log(`No more people on channel, Unsubscribe server from ${channel}`)
+        // NOTE: This is now disabled because of a rare error happening in the Redis client, see https://github.com/redis/node-redis/issues/2417 on a similar issue, you can fix this modifying the redis library
+        //this.redisPubSubClient.sUnsubscribe(channel);
+        console.log(`Unsubscribed server from ${channel}`);
+        this.channelSubscriptions.delete(channel);
+      });
 
-    console.log(`User ${userID} disconnected`);
+      console.log(`User ${userID} disconnected`);
+    } catch (err) {
+      console.log("Error in removeWebsocket: " + err);
+    }
   }
 }
 
